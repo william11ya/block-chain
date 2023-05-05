5b7ed9c7bbb14bbd217d5b4785a687feef81ab4e
william11ya <114579217+william11ya@users.noreply.github.com>
2023-05-05 22:48:51 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,72 @@
+# from flask import Flask, render_template
+# from web3 import Web3
+# import json
+
+# # 建立與 Ganache 本地區塊鏈的連接
+# w3 = Web3(Web3.HTTPProvider('http://localhost:7545'))
+
+# # 載入 Solidity 智能合約 ABI
+# with open('bin/contracts/SimpleStorage.json') as f:
+#     abi = json.load(f)['abi']
+
+# # 設定智能合約地址
+# contract_address = '0x72E6E4a21B68Efed620001Eda0F4bdC4df158d0B'
+
+# app = Flask(__name__)
+
+# # 建立智能合約實例
+# contract = w3.eth.contract(address=contract_address, abi=abi)
+
+
+# # 定義一個 API 接口，用於設置狀態變量的值
+# @app.route('/set_value/<value>')
+# def set_value(value):
+#     tx_hash = contract.functions.setValue(value).transact()
+#     w3.eth.waitForTransactionReceipt(tx_hash)
+#     return 'Value set to ' + value
+
+# # 定義一個 API 接口，用於讀取狀態變量的值
+# @app.route('/get_value')
+# def get_value():
+#     value = contract.functions.value().call()
+#     return 'Value is ' + str(value)
+
+# # 定義一個路由，用於顯示狀態變量的值
+# @app.route('/')
+# def index():
+#     value = contract.functions.value().call()
+#     return render_template('index.html', value=value)
+
+# @app.errorhandler(500)
+# def server_error(e):
+#     print(e)
+#     return 'An internal server error occurred.', 500
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+import json
+from web3 import Web3
+# Fill in your infura API key here
+w3 = Web3(Web3.HTTPProvider('HTTP://localhost:7545'))
+
+w3.eth.default_account = w3.eth.accounts[0]
+
+compiled_contract_path = 'build/contracts/gfg.json'
+
+deployed_contract_address = '0x7A2377407203A779421e586935Eb293e04389C4f'
+
+with open(compiled_contract_path) as file:
+    contract_json = json.load(file)
+    contract_abi = contract_json['abi']
+
+contract = w3.eth.contract(
+    address = deployed_contract_address, abi = contract_abi)
+
+output = contract.functions.geeks().call()
+ 
+print(type(output))
+print(output)
+
+print(w3.is_connected())
+
---
+++ contracts/Vis.sol
@@ -0,0 +1,10 @@
+
+pragma solidity >= 0.5.0 < 0.9.0;
+ 
+contract gfg{
+  function geeks() public pure returns (uint)
+  {
+    return 1;
+  }
+
+}
